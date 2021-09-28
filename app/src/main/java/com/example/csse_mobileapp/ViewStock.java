3398e049114948df60b31cdae1fcee3fc142a1cb
TharushiRachel <68675238+TharushiRachel@users.noreply.github.com>
2021-09-28 19:18:19 +0530

    Site Manager Interfaces done

--- app/src/main/java/com/example/csse_mobileapp/ViewStock.java
+++ app/src/main/java/com/example/csse_mobileapp/ViewStock.java
@@ -2,7 +2,9 @@ package com.example.csse_mobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewStock extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ViewStock extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_stock);
+    }
+
+    public void sendMessage1(View view) {
+        Intent intent = new Intent(this, PurchaseOrder.class);
+        startActivity(intent);
     }
 }
